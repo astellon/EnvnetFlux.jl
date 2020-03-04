6382f00b3a6df2a233e2f56ac57dbdfb26980b65
astellon <22632787+astellon@users.noreply.github.com>
2020-03-04 13:21:37 +0900

    Use GPU

--- src/main.jl
+++ src/main.jl
@@ -7,8 +7,22 @@ using DataLoaders
 include("envnet.jl")
 include("esc50.jl")
 
+function mytrain!(loss, ps, data, opt)
+  ps = Flux.Params(ps)
+  for (x, y) in data
+    x = x |> gpu
+    y = y |> gpu
+    gs = gradient(ps) do
+      training_loss = loss(x, y)
+      return training_loss
+    end
+    println("train loss: $(loss(x, y))")
+    Flux.update!(opt, ps, gs)
+  end
+end
+
 function main()
-  model = Envnetv2(50)
+  model = Envnetv2(50) |> gpu
 
   datapath = "/home/astellon/workspace/python/envnet-pytorch/ESC-50-master/audio"
   traindataset = ESC50Dataset(datapath, [1,2,3])
@@ -24,11 +38,7 @@ function main()
     crossentropy(ỹ, y)
   end
 
-  function cb()
-    println("training")
-  end
-
-  @epochs 2 Flux.train!(softmax_cross_entropy, params(model), trainloader, optimizer, cb = cb)
+  @epochs 2 mytrain!(softmax_cross_entropy, params(model), trainloader, optimizer)
 end
 
 main()
